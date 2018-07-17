dc1da067934b35556c02e398c5136d22661afe70
unknown <yogesh.iparkar@gmail.com>
2018-07-17 13:42:41 +0530

    Sample App

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './index.css'
+class App extends Component
+{
+    render(){
+    return(
+        <div className='person'>
+            <h1>Yogesh</h1>
+        </div>
+    );
+}
+}
+export default App;
