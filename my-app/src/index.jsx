2711c88fe2fc7919486a4a0a70fb706fc6d42b79
unknown <yogesh.iparkar@gmail.com>
2018-07-17 10:32:49 +0530

    Sample App commit test to git

---
+++ my-app/src/index.jsx
@@ -0,0 +1,10 @@
+function Person()
+{
+    return(
+        <div className='person'>
+            <h1>Yogesh</h1>
+        </div>
+    );
+}
+
+ReactDom.render(<Person/>, document.getElementById('p1')); 
